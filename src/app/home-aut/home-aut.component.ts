df46c540509f4e8c6cf097671464052345ff0bfb
gabriel25115CG <gabriel.utbm.fr>
2024-03-05 17:53:38 +0100

    Fix bug Aut

--- src/app/home-aut/home-aut.component.ts
+++ src/app/home-aut/home-aut.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importer le service Router
+import { FirebaseService } from '../firebase.service';
 
 @Component({
   selector: 'app-home-aut',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-aut.component.css']
 })
 export class HomeAUTComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit(): void {
+    this.firebaseService.isLoggedIn().subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+      if (!this.isLoggedIn) {
+        // Rediriger vers la page de connexion si l'utilisateur n'est pas authentifié
+        this.router.navigate(['/login']); // Utiliser le service Router pour naviguer vers la page de connexion
+      }
+    });
   }
-
 }
